ae8962dbcd072509f683b7bfe6f37a3d1b159707
rclauson <rclauson@google.com>
2016-05-16 09:50:13 -0400

    Done.

--- cartoon_collections.js
+++ cartoon_collections.js
@@ -1,9 +1,21 @@
-function dwarfRollCall(dwarves) {
-	//your code here!
-}
+var i = 0;
+var dwarvesList = "";
 
+function dwarfRollCall(dwarves) {
+ while (i < dwarves.length) {
+   dwarvesList = dwarvesList + ((i+1) + ". " + dwarves[i] + " ");
+   i=i+1;
+ }
+ return dwarvesList;
+}
+  var i=0;
+  var planeteerCallsList ="";
 function summonCaptainPlanet(planeteerCalls){
-	//your code here!
+
+  for(var i=0; i<planeteerCalls.length; i=i +1){
+  planeteerCalls[i] = planeteerCalls[i] +"!";
+  }
+  return #=> planeteerCalls.toUpperCase
 }
 
 function longPlaneteerCalls(words) {
